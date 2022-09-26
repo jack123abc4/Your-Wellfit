ac2093941ecd805806f032f441f278b5077ec4ad
rpjtarnofsky <110841253+rpjtarnofsky@users.noreply.github.com>
2022-09-26 13:23:45 -0400

    models/controllers

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const recipeRoutes = require('./recipeRoutes');
 const userRoutes = require('./userRoutes');
+const workoutRoutes = require('./workoutRoutes')
 
 router.use('/recipes', recipeRoutes);
 router.use('/users', userRoutes);
+router.use('/workouts', workoutRoutes)
+
 
 module.exports = router;
