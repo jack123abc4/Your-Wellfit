3ec489096241e84a22332875e92e89e8d63f286e
Lauren <lburke522@gmail.com>
2022-09-29 14:40:20 -0400

    modals and fixed routing

--- public/js/login.js
+++ public/js/login.js
@@ -1,9 +1,11 @@
+
+
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
   // Collect values from the login form
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  const email = document.querySelector('#inputEmail').value.trim();
+  const password = document.querySelector('#inputPassword').value.trim();
 
   if (email && password) {
     // Send a POST request to the API endpoint
@@ -25,9 +27,9 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#name-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+  const name = document.querySelector('#signupUsername').value.trim();
+  const email = document.querySelector('#signupEmail').value.trim();
+  const password = document.querySelector('#signupPassword').value.trim();
 
   if (name && email && password) {
     const response = await fetch('/api/users', {
