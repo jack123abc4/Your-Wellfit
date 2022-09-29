5e17109e7b84525385e27276cabb12860bbb4454
jack123abc4 <jack.r.motherway@gmail.com>
2022-09-29 01:08:50 -0400

    clicking ingredient sends put request, flips state in db and html

--- controllers/api/ingredientRoutes.js
+++ controllers/api/ingredientRoutes.js
@@ -4,17 +4,20 @@ const sequelize = require('../../config/connection');
 const { Ingredient } = require('../../models');
 
 router.put('/:id', async (req, res) => {
-    try{
+    // try{
         const ingredientData = await Ingredient.update(req.body, {
             where: {
                 id: req.params.id,
             }
         });
-        res.status(200).json(ingredientData);
-    }
-    catch (err) {
-        res.status(500).json(err);
-    }
+        const ingredient = await Ingredient.findByPk(req.params.id);
+        // console.log(ingredient.get());
+        // console.log(ingredient.get({plain:true}));
+        res.status(200).json(ingredient.get());
+    // }
+    // catch (err) {
+    //     res.status(500).json(err);
+    // }
     
 
 })
--- public/js/recipe.js
+++ public/js/recipe.js
@@ -1,5 +1,17 @@
 const ingredientClick = async (event) => {
     console.log(event.target);
+    const ingredientID = event.target.getAttribute("id").split("-")[3];
+    console.log("ID:",ingredientID);
+    const ingredientState = event.target.getAttribute("state");
+    console.log("State:",ingredientState);
+    const response = await fetch(`/api/ingredients/${ingredientID}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({active: ingredientState === "active" ? false : true})
+        
+      });
+    console.log(response.json());
+    event.target.setAttribute("state", ingredientState === "active" ? "inactive" : "active")
         
 }
   
